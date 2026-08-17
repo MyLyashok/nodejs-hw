78f68b7b9854435e798246886a5462e0c720a76e
Mykola Lyashok <lyashok100@gmail.com>
2026-08-17 14:23:13 +0300

    final

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import pinoLogger from 'pino-http';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoLogger());
+
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
@@ -14,6 +21,28 @@ app.get('/notes/:noteId', (req, res) => {
     message: `Retrieved note with ID: ${req.params.noteId}`
   });
 });
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    "message": "Route not found"
+  });
+
+});
+
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: `${err.message}`,
+  });
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
