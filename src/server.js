0dd7066b1e5e58eacc6d733a0d17e9604f9964dd
Mykola Lyashok <lyashok100@gmail.com>
2026-09-06 16:15:22 +0300

    finish

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,27 +15,21 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
-
-app.get('/notes/:noteId', (req, res) => {
-  res.status(200).json({
-    message: `Retrieved note with ID: ${req.params.noteId}`,
-  });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+app.use(notesRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-await connectMongoDB();
+const startServer = async () => {
+  try {
+    await connectMongoDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error.message);
+  }
+};
+
+startServer();
